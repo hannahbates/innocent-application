43c4de8a8ca706481accc54377c410a70fd6dcf9
Dan Edwards <igneosaur@gmail.com>
2013-06-30 17:57:47 +0100

    Adding main functionality.

--- js/main.js
+++ js/main.js
@@ -11,17 +11,60 @@
     'use-strict';
 
     /* Private vars */
-    var WOTD_FEED_URL = 'http://www.merriam-webster.com/word/index.xml';
+    var smoothieHeight = $(window).height();
+    var smoothiesData = [];
+    var activeSmoothie = 0;
 
-    /* Public functions */
+    /* Functions */
     function init(callback) {
         setSmoothieHeights();
+        setSmoothiesData();
+
+        // Document listeners.
+        $(window).scroll(function() {
+            var scrollPosition = $(window).scrollTop();
+
+            for(var i = 0; i < smoothiesData.length; i++) {
+                if(scrollPosition > smoothiesData[i].threshold.top
+                   && scrollPosition < smoothiesData[i].threshold.bottom
+                   && activeSmoothie !== i) {
+                    activeSmoothie = i;
+                    $('.smoothie').removeClass('active');
+                    smoothiesData[i].element.addClass('active');
+                    highlightIngredients(smoothiesData[i].ingredients);
+                    break;
+                }
+            }
+        });
     }
 
     function setSmoothieHeights() {
-        var viewportHeight = $(window).height();
-        console.debug(viewportHeight);
-        $('.smoothies div').css('height', viewportHeight);
+        $('.smoothie').css('height', smoothieHeight);
+    }
+
+    function setSmoothiesData() {
+        $('.smoothie').each(function(i) {
+            var $smoothie = $(this);
+            var smoothieIngredients = $smoothie.data('ingredients') ? $smoothie.data('ingredients').split(' ') : false;
+            
+            var thisSmoothieData = {
+                element : $smoothie,
+                ingredients : smoothieIngredients,
+                threshold : {
+                    top : $smoothie.offset().top - ($smoothie.height()/2),
+                    bottom : $smoothie.height() + $smoothie.offset().top - ($smoothie.height()/2)
+                }
+            };
+            
+            smoothiesData.push(thisSmoothieData);
+        });
+    }
+    
+    function highlightIngredients(ingredients) {
+        $('.ingredients li').removeClass('active');
+        for(var i = 0; i < ingredients.length; i++) {
+            $('.ingredients .' + ingredients[i]).addClass('active');
+        }
     }
 
     init();
