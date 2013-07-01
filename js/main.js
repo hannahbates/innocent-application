db2b4102283dcc7a59f9e0b74d68e177db7dfa2b
Dan Edwards <igneosaur@gmail.com>
2013-07-01 01:47:52 +0100

    JSLinting.

--- js/main.js
+++ js/main.js
@@ -1,70 +1,78 @@
 /*!
- * Igneosaur JavaScript Library v0.1
+ * Igneosaur JavaScript Library v1
  * http://igneosaur.co.uk/
  *
- * Copyright 2012 Igneosaur
+ * Copyright 2013 Igneosaur
  * Released under the MIT license
  *
- * Date: Wed Oct 10 2012
+ * Date: Mon Jul 01 2013
  */
 (function ($) {
-    'use-strict';
+    'use strict';
 
-    /* Private vars */
-    var smoothieHeight = $(window).height();
-    var smoothiesData = [];
-    var activeSmoothie = 0;
+    /*jslint browser: true*/
+    /*global $, jQuery*/
+
+    /* Vars */
+    var smoothieHeight = $(window).height(),
+        smoothiesData = [],
+        activeSmoothie = 0,
+    /* DOM elements */
+        INGREDIENTS_SELECTOR = '.ingredients',
+        SMOOTHIE_SELECTOR = '.smoothie';
 
     /* Functions */
+    function highlightIngredients(ingredients) {
+        var i = 0;
+
+        $(INGREDIENTS_SELECTOR + ' li').removeClass('active');
+
+        for (i; i < ingredients.length; i += 1) {
+            $(INGREDIENTS_SELECTOR + ' .' + ingredients[i]).addClass('active');
+        }
+    }
+
+    function setSmoothieHeights() {
+        $(SMOOTHIE_SELECTOR).css('height', smoothieHeight);
+    }
+
+    function setSmoothiesData() {
+        $(SMOOTHIE_SELECTOR).each(function (i) {
+            var $smoothie = $(this),
+                smoothieIngredients = $smoothie.data('ingredients') ? $smoothie.data('ingredients').split(' ') : false,
+                thisSmoothieData = {
+                    element : $smoothie,
+                    ingredients : smoothieIngredients,
+                    threshold : {
+                        top : $smoothie.offset().top - ($smoothie.height() / 2),
+                        bottom : $smoothie.height() + $smoothie.offset().top - ($smoothie.height() / 2)
+                    }
+                };
+
+            smoothiesData.push(thisSmoothieData);
+        });
+    }
+
     function init(callback) {
         setSmoothieHeights();
         setSmoothiesData();
 
         // Document listeners.
-        $(window).scroll(function() {
-            var scrollPosition = $(window).scrollTop();
+        $(window).scroll(function () {
+            var i = 0, scrollPosition = $(window).scrollTop();
 
-            for(var i = 0; i < smoothiesData.length; i++) {
-                if(scrollPosition > smoothiesData[i].threshold.top
-                   && scrollPosition < smoothiesData[i].threshold.bottom
-                   && activeSmoothie !== i) {
+            for (i; i < smoothiesData.length; i += 1) {
+                if (scrollPosition > smoothiesData[i].threshold.top
+                        && scrollPosition < smoothiesData[i].threshold.bottom
+                        && activeSmoothie !== i) {
                     activeSmoothie = i;
-                    $('.smoothie').removeClass('active');
+                    $(SMOOTHIE_SELECTOR).removeClass('active');
                     smoothiesData[i].element.addClass('active');
                     highlightIngredients(smoothiesData[i].ingredients);
                     break;
                 }
             }
         });
-    }
-
-    function setSmoothieHeights() {
-        $('.smoothie').css('height', smoothieHeight);
-    }
-
-    function setSmoothiesData() {
-        $('.smoothie').each(function(i) {
-            var $smoothie = $(this);
-            var smoothieIngredients = $smoothie.data('ingredients') ? $smoothie.data('ingredients').split(' ') : false;
-            
-            var thisSmoothieData = {
-                element : $smoothie,
-                ingredients : smoothieIngredients,
-                threshold : {
-                    top : $smoothie.offset().top - ($smoothie.height()/2),
-                    bottom : $smoothie.height() + $smoothie.offset().top - ($smoothie.height()/2)
-                }
-            };
-            
-            smoothiesData.push(thisSmoothieData);
-        });
-    }
-    
-    function highlightIngredients(ingredients) {
-        $('.ingredients li').removeClass('active');
-        for(var i = 0; i < ingredients.length; i++) {
-            $('.ingredients .' + ingredients[i]).addClass('active');
-        }
     }
 
     init();
